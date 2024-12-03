07dd2c0f7addf1855d1a4d987f80899c89750e06
Syiin <xbio.oo@gmail.com>
2024-12-03 18:52:09 +0800

    connect to amqp

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,23 @@
 package main
 
+import "os"
+import "os/signal"
 import "fmt"
+import "log"
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	connection_str := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(connection_str)
+	if err != nil {
+		log.Fatal("Could not connect to server")
+	}
+	fmt.Println("Connection successful!")
+	defer connection.Close()
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
 	fmt.Println("Starting Peril server...")
 }
