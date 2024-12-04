c09420a84ad55a65b59b1d73383ccd54fbf6a01e
Syiin <xbio.oo@gmail.com>
2024-12-04 19:17:39 +0800

    refactor and rename

--- internal/pubsub/json.go
+++ internal/pubsub/json.go
@@ -2,8 +2,6 @@ package pubsub
 
 import "context"
 import "encoding/json"
-import "log"
-import "fmt"
 import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -15,50 +13,4 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 		Body:        jsonBytes,
 	}
 	return ch.PublishWithContext(context.Background(), exchange, key, false, false, msg)
-}
-
-const (
-	Durable int = iota
-	Transient
-)
-
-func DeclareAndBind(
-	conn *amqp.Connection,
-	exchange,
-	queueName,
-	key string,
-	simpleQueueType int, // an enum to represent "durable" or "transient"
-) (*amqp.Channel, amqp.Queue, error) {
-	channel, err := conn.Channel()
-	if err != nil {
-		log.Fatal("Could not open channel")
-	}
-
-	var durable, autoDelete, exclusive bool
-
-	switch simpleQueueType {
-	case Durable:
-		fmt.Println("Durable...")
-		durable = true
-		autoDelete = false
-		exclusive = false
-	case Transient:
-		fmt.Println("Transient...")
-		durable = false
-		autoDelete = true
-		exclusive = true
-	default:
-		fmt.Println("Unknown simpleQueueType value:", simpleQueueType)
-	}
-	queue, err := channel.QueueDeclare(queueName, durable, autoDelete, exclusive, false, amqp.Table{})
-	if err != nil {
-		log.Fatal("Could not declare queue")
-	}
-
-	err = channel.QueueBind(queueName, key, exchange, false, amqp.Table{})
-	if err != nil {
-		log.Fatal("Could not bind queue: %v", err)
-	}
-	fmt.Println("Queue Type:", simpleQueueType, ", AutoDelete:", autoDelete, ", Exclusive:", exclusive)
-	return channel, queue, err
 }
