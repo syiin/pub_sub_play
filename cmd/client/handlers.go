d5b4abcadd21bd46b386e1fe1a86bbc2f5f4ad7a
Syiin <xbio.oo@gmail.com>
2024-12-06 17:37:23 +0800

    separate into handlers file

---
+++ cmd/client/handlers.go
@@ -0,0 +1,70 @@
+package main
+
+import "fmt"
+import "log"
+import "github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+import "github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+import "github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) pubsub.AckType {
+	return func(ps routing.PlayingState) pubsub.AckType {
+		defer fmt.Print("> ")
+		gs.HandlePause(ps)
+		return pubsub.Ack
+	}
+}
+
+func handlerMove(gs *gamelogic.GameState, channel *amqp.Channel) func(gamelogic.ArmyMove) pubsub.AckType {
+	return func(move gamelogic.ArmyMove) pubsub.AckType {
+		defer fmt.Print("> ")
+		moveOutcome := gs.HandleMove(move)
+
+		switch moveOutcome {
+		case gamelogic.MoveOutComeSafe:
+			return pubsub.Ack
+		case gamelogic.MoveOutcomeMakeWar:
+			err := pubsub.PublishJSON(
+				channel,
+				routing.ExchangePerilTopic,
+				routing.WarRecognitionsPrefix+"."+gs.GetUsername(),
+				gamelogic.RecognitionOfWar{
+					Attacker: move.Player,
+					Defender: gs.GetPlayerSnap(),
+				},
+			)
+			if err != nil {
+				fmt.Println("Unknown move occurred")
+			}
+			return pubsub.NackRequeue
+		case gamelogic.MoveOutcomeSamePlayer:
+			return pubsub.NackDiscard
+		default:
+			return pubsub.NackDiscard
+		}
+	}
+}
+
+func handlerWar(gs *gamelogic.GameState) func(gamelogic.RecognitionOfWar) pubsub.AckType {
+	return func(rw gamelogic.RecognitionOfWar) pubsub.AckType {
+		defer fmt.Print("> ")
+		outcome, _, _ := gs.HandleWar(rw)
+		switch outcome {
+		case gamelogic.WarOutcomeNotInvolved:
+			return pubsub.NackRequeue
+		case gamelogic.WarOutcomeNoUnits:
+			return pubsub.NackDiscard
+		case gamelogic.WarOutcomeOpponentWon:
+			return pubsub.Ack
+		case gamelogic.WarOutcomeYouWon:
+			return pubsub.Ack
+		case gamelogic.WarOutcomeDraw:
+			return pubsub.Ack
+		default:
+			log.Fatal("Received bad war outcome", outcome)
+			return pubsub.NackDiscard
+		}
+	}
+}
